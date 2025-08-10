198198601ef73e37285bfdc583091b06c7438c27
Equiel-1703 <henriquegabriel22.hg@gmail.com>
2025-08-10 19:18:51 -0300

    Refactor OpenCL version definition and improve documentation

--- c_src/cldef.hpp
+++ c_src/cldef.hpp
@@ -1,6 +1,12 @@
+/*
+    Defines the OpenCL version to use and include the OpenCL headers
+*/
+
 #pragma once
 
-// Defines the OpenCL version to use and include the OpenCL headers
+#define OPENCL_VERSION 200 // We are going to be using OpenCL 2.0
 
-#define CL_HPP_TARGET_OPENCL_VERSION 200 // OpenCL 2.0
+#define CL_TARGET_OPENCL_VERSION OPENCL_VERSION
+#define CL_HPP_TARGET_OPENCL_VERSION OPENCL_VERSION
+
 #include <CL/cl.hpp>
