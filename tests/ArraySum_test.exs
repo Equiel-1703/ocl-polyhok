d3b7624638c2258445a1124f53a90f02c92cb3d7
Equiel-1703 <henriquegabriel22.hg@gmail.com>
2025-08-13 17:40:21 -0300

    Improves ArraySum test script

--- tests/ArraySum_test.exs
+++ tests/ArraySum_test.exs
@@ -1,40 +1,43 @@
 require OCLPolyHok
 
 OCLPolyHok.defmodule ArraySum do
-  defk sum_ker(a1, a2, size) do
+  defk sum_ker(a1, a2, result_array, size) do
     index = blockIdx.x * blockDim.x + threadIdx.x
     stride = blockDim.x * gridDim.x
 
-    sum = 0.0
     for i in range(index, size, stride) do
-      sum = sum + a1[i]
+      result_array[i] = a1[i] + a2[i]
     end
   end
 
-  def sum(input) do
+  def sum(input_1, input_2) do
     IO.puts "ArraySum.sum called"
-    shape = OCLPolyHok.get_shape(input)
-    type = OCLPolyHok.get_type(input)
+
+    # Getting shape and type of the input tensors
+    shape = OCLPolyHok.get_shape(input_1)
+    type = OCLPolyHok.get_type(input_1)
+
+    # Creating a new GPU tensor to hold the result
     result_gpu = OCLPolyHok.new_gnx(shape, type)
 
+    # Calculating the number of blocks based on the size of the input
     size = Tuple.product(shape)
     threadsPerBlock = 128
     numberOfBlocks = div(size + threadsPerBlock - 1, threadsPerBlock)
 
-    OCLPolyHok.spawn(&ArraySum.sum_ker/3,
+    OCLPolyHok.spawn(&ArraySum.sum_ker/4,
               {numberOfBlocks, 1, 1},
               {threadsPerBlock, 1, 1},
-              [input, result_gpu, size])
+              [input_1, input_2, result_gpu, size]) # Kernel parameters
+
     result_gpu
   end
 
 end
 
-a = Nx.tensor(Enum.to_list(1..100), type: {:f, 32})
+a = Nx.tensor(Enum.to_list(1..100), type: {:f, 32}) |> OCLPolyHok.new_gnx
+b = Nx.tensor(Enum.to_list(1..100), type: {:f, 32}) |> OCLPolyHok.new_gnx
 
-result = a
-    |> OCLPolyHok.new_gnx
-    |> ArraySum.sum()
-    |> OCLPolyHok.get_gnx
+result = ArraySum.sum(a, b) |> OCLPolyHok.get_gnx
 
-IO.inspect(result, limit: :infinity)
+IO.inspect(result, limit: :infinity, label: "Result of ArraySum.sum: ")
