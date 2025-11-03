3ae2199910cc9b07990ee6434e3214c5f5ab047a
Equiel-1703 <henriquegabriel22.hg@gmail.com>
2025-11-03 20:34:42 -0300

    Refactor OpenCL dot product benchmark to use cl::Platform and cl::Device for better context management; add ocl_benchs.hpp for utility functions.

--- benchmarks/opencl/dot_product.cpp
+++ benchmarks/opencl/dot_product.cpp
@@ -1,9 +1,9 @@
-#include "../../c_src/ocl_interface/OCLInterface.hpp"
-
 #include <iostream>
 #include <cstdlib>
 #include <cstdio>
 #include <chrono>
+
+#include "ocl_benchs.hpp"
 
 std::string opencl_kernel_code = R"CLC(
 inline float cas_float(volatile __global float *address, float oldv, float newv)
@@ -74,13 +74,16 @@ __kernel void reduce_kernel(__global float *a, __global float *ref4, float initi
 
 int main(int argc, char *argv[])
 {
-  OCLInterface ocl(true);
-  ocl.selectDefaultPlatform();
-  ocl.selectDefaultDevice(CL_DEVICE_TYPE_GPU);
+  cl::Platform platform = getDefaultPlatform();
+  cl::Device device = getDefaultDevice(platform);
+  cl::Context context(device);
 
-  cl::Program program = ocl.createProgram(opencl_kernel_code);
-  cl::Kernel map_2kernel = ocl.createKernel(program, "map_2kernel");
-  cl::Kernel reduce_kernel = ocl.createKernel(program, "reduce_kernel");
+  // Create a queue with profiling enabled, this is needed to measure execution time of kernels
+  cl::CommandQueue queue(context, device, CL_QUEUE_PROFILING_ENABLE);
+
+  cl::Program program(context, opencl_kernel_code, true);
+  cl::Kernel map_2kernel(program, "map_2kernel");
+  cl::Kernel reduce_kernel(program, "reduce_kernel");
 
   float *a, *b, *resp;
 
@@ -113,7 +116,7 @@ int main(int argc, char *argv[])
 
   float *final = (float *)malloc(sizeof(float));
   final[0] = 0;
-  
+
   int threadsPerBlock = 256;
   int numberOfBlocks = (N + threadsPerBlock - 1) / threadsPerBlock;
 
@@ -121,13 +124,12 @@ int main(int argc, char *argv[])
   cl::NDRange global_range(numberOfBlocks * threadsPerBlock);
   cl::NDRange local_range(threadsPerBlock);
 
-  // Start measuring time
   auto start = std::chrono::high_resolution_clock::now();
 
-  cl::Buffer buffer_a = ocl.createBuffer(N * sizeof(float), CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, a);
-  cl::Buffer buffer_b = ocl.createBuffer(N * sizeof(float), CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, b);
-  cl::Buffer buffer_resp = ocl.createBuffer(N * sizeof(float), CL_MEM_READ_WRITE);
-  cl::Buffer d_final = ocl.createBuffer(sizeof(float), CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, final);
+  cl::Buffer buffer_a(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, N * sizeof(float), a);
+  cl::Buffer buffer_b(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, N * sizeof(float), b);
+  cl::Buffer buffer_resp(context, CL_MEM_READ_WRITE, N * sizeof(float));
+  cl::Buffer d_final(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, sizeof(float), final);
 
   map_2kernel.setArg(0, buffer_a);
   map_2kernel.setArg(1, buffer_b);
@@ -139,15 +141,31 @@ int main(int argc, char *argv[])
   reduce_kernel.setArg(2, 0.0f);
   reduce_kernel.setArg(3, N);
 
+  // Measure execution time of kernels with event profiling
+  cl::Event map_event, reduce_event, read_event;
+
   // Run kernels
-  ocl.executeKernel(map_2kernel, global_range, local_range);
-  ocl.executeKernel(reduce_kernel, global_range, local_range);
+  queue.enqueueNDRangeKernel(map_2kernel, cl::NullRange, global_range, local_range, nullptr, &map_event);
+  queue.enqueueNDRangeKernel(reduce_kernel, cl::NullRange, global_range, local_range, nullptr, &reduce_event);
 
-  ocl.readBuffer(d_final, final, sizeof(float));
+  // Read back the result
+  queue.enqueueReadBuffer(d_final, CL_TRUE, 0, sizeof(float), final, nullptr, &read_event);
 
-  auto end =  std::chrono::high_resolution_clock::now();
+  // Wait for all operations to finish
+  queue.finish();
+
+  auto end = std::chrono::high_resolution_clock::now();
   std::chrono::duration<double, std::milli> elapsed = end - start;
-  double time = elapsed.count();
+
+  // Calculate total time using event profiling info
+  cl_ulong map_start = map_event.getProfilingInfo<CL_PROFILING_COMMAND_START>();
+  cl_ulong map_end = map_event.getProfilingInfo<CL_PROFILING_COMMAND_END>();
+  cl_ulong reduce_start = reduce_event.getProfilingInfo<CL_PROFILING_COMMAND_START>();
+  cl_ulong reduce_end = reduce_event.getProfilingInfo<CL_PROFILING_COMMAND_END>();
+  cl_ulong read_start = read_event.getProfilingInfo<CL_PROFILING_COMMAND_START>();
+  cl_ulong read_end = read_event.getProfilingInfo<CL_PROFILING_COMMAND_END>();
+
+  double time = ((map_end - map_start) + (reduce_end - reduce_start) + (read_end - read_start)) / 1e6; // Convert to milliseconds
 
   printf("OpenCL\t%d\t%3.1f\n", N, time);
   printf("Result: %f\n", final[0]);
@@ -157,6 +175,9 @@ int main(int argc, char *argv[])
   printf("-------------------------\n");
   printf("Global range: %lu\n", global_range[0]);
   printf("Local range: %lu\n", local_range[0]);
+  printf("-------------------------\n");
+  printf("Elapsed time (chrono): %3.5f ms\n", elapsed.count());
+  printf("Elapsed time (profiling): %3.5f ms\n", time);
 
   free(a);
   free(b);
