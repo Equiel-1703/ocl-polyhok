f72640cca4dd370bd24be2ba69c395b5bb1d9f07
Equiel-1703 <henriquegabriel22.hg@gmail.com>
2026-04-10 17:59:11 -0300

    Refactor aligned Nx tensor test script

--- tests/AlignedNxes.exs
+++ tests/AlignedNxes.exs
@@ -1,16 +1,11 @@
 require OCLPolyHok
 
-IO.puts("\n---Testando new_nx---\n")
+OCLPolyHok.set_debug_logs(true)
 
-nx_f_1 = OCLPolyHok.new_nx(1, 3, {:f, 32})
+IO.puts("\n---Testando OCLPolyHok.tensor/2---\n")
+
+nx_f_1 = OCLPolyHok.tensor([1.0, 2.0, 3.0], type: {:f, 32})
+
+OCLPolyHok.is_nx_aligned?(nx_f_1)
 
 IO.inspect(nx_f_1, label: "Nx com 1 linha e 3 colunas, tipo float 32")
-
-IO.puts("Inserindo valores no Nx...")
-
-indices = Nx.tensor([[0, 0], [0, 1], [0, 2]])
-values = Nx.tensor([1.5, 2.5, 3.5], type: {:f, 32})
-
-new_nx = Nx.indexed_put(nx_f_1, indices, values)
-
-IO.inspect(new_nx, label: "Nx atualizado com os valores inseridos")
