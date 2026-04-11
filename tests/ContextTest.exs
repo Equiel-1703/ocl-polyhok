69d1e6ae32f8c4242756651bcdae9090acd06618
Equiel-1703 <henriquegabriel22.hg@gmail.com>
2026-04-11 17:25:01 -0300

    Updated context test script

--- tests/ContextTest.exs
+++ tests/ContextTest.exs
@@ -13,27 +13,24 @@ OCLPolyHok.defmodule ArraySum do
 end
 
 cpu_ctx = OCLPolyHok.cpu()
-IO.inspect(cpu_ctx, label: "Context created")
 
-arr_1 = Nx.tensor([1,2,3], type: {:s, 32})
-IO.inspect(arr_1, label: "Original Nx Tensor 1")
+IO.puts("Created CPU context: #{inspect(cpu_ctx)}")
 
-arr_2 = Nx.tensor([2,4,6], type: {:s, 32})
-IO.inspect(arr_2, label: "Original Nx Tensor 2")
+arr_1 = OCLPolyHok.tensor([1,2,3], type: {:s, 32})
+arr_2 = OCLPolyHok.tensor([2,4,6], type: {:s, 32})
+arr_res = OCLPolyHok.tensor([0,0,0], type: {:s, 32})
 
-res_arr = OCLPolyHok.with cpu_ctx do
-  dev_arr_1 = OCLPolyHok.new_gnx(arr_1)
-  dev_arr_2 = OCLPolyHok.new_gnx(arr_2)
-  dev_res_arr = OCLPolyHok.new_gnx({1,3}, {:s, 32})
+IO.puts("Input Array 1: #{inspect(arr_1)}")
+IO.puts("Input Array 2: #{inspect(arr_2)}")
+IO.puts("Result Array before computation: #{inspect(arr_res)}")
 
+OCLPolyHok.with cpu_ctx do
   OCLPolyHok.spawn(
       &ArraySum.sum_ker/4,
       {1, 1, 1},
       {3, 1, 1},
-      [dev_arr_1, dev_arr_2, dev_res_arr, 3]
+      [arr_1, arr_2, arr_res, 3]
   )
-
-  OCLPolyHok.get_gnx(dev_res_arr)
 end
 
-IO.inspect(res_arr, label: "Result array after kernel execution")
+IO.puts("Result Array after computation: #{inspect(arr_res)}")
